3df998e7ee58daa28d08a1eeff2fb71d987325e0
Jacob Deinum <jdeinum@nullspaces.org>
2026-06-19 14:12:35 -0600

    changelog said this will change soon

--- lua/config/conform.lua
+++ lua/config/conform.lua
@@ -5,7 +5,7 @@ M.keys = {
 		"<leader>cf",
 		function()
 			require("conform").format({
-				lsp_fallback = true,
+				lsp_format = "fallback",
 				async = false,
 				timeout_ms = 500,
 			})
@@ -36,7 +36,7 @@ M.config = function()
 			hcl = { "hclfmt" },
 		},
 		format_on_save = {
-			lsp_fallback = true,
+			lsp_format = "fallback",
 			timeout_ms = 500,
 		},
 	})
